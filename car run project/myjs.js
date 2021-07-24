7d9cc8c6e34a9fa7001b7746e8afd7513127c125
SHALINI PRIYA <84949535+SHALINIPRIYA21@users.noreply.github.com>
2021-07-24 18:18:30 +0530

    Add files via upload

---
+++ car run project/myjs.js
@@ -0,0 +1,36 @@
+var y;
+m=0;
+function start()
+{
+	 y=setInterval(run,100);
+	
+	function run(){
+		if(m==1200){
+			clearInterval(y);
+			m=0;
+		}
+		else{
+			m+=10;
+		var x=document.getElementById("img");
+		x.style.marginLeft=m+'px';
+		}
+	}
+}
+ function stop()
+ {
+	 clearInterval(y)
+ }
+ 
+ 
+ 
+ 
+ var audio =document.createElement('audio');
+ audio.setAttribute('src','song.wav');
+ 
+ function playsound(){
+	 audio.play();
+ }
+ 
+ function pausesound() {
+	 audio.pause();
+ }
